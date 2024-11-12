67c883444f7c6734c5e2cbb8373dfa9b556f7c5d
hindhuja2005 <151657743+hindhuja2005@users.noreply.github.com>
2024-11-12 21:47:47 +0530

    Update 2179-most-beautiful-item-for-each-query.py

--- 2179-most-beautiful-item-for-each-query/2179-most-beautiful-item-for-each-query.py
+++ 2179-most-beautiful-item-for-each-query/2179-most-beautiful-item-for-each-query.py
@@ -12,20 +12,3 @@ class Solution(object):
                 j+=1
             res[i]=max_b
         return res
-
-
-
-
-
-class Solution:
-    def maximumBeauty(self, items: List[List[int]], queries: List[int]) -> List[int]:
-        ans=[]
-        items=sorted(items,key=lambda x:x[1],reverse=True)
-        for i in queries:
-            for j in items:
-                if j[0]<=i:
-                    ans.append(j[1])
-                    break
-            else:
-                ans.append(0)
-        return ans
