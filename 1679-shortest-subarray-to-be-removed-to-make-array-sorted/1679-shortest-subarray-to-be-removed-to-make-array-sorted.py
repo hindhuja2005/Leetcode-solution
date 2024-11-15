6aac1292065f69eb2ee18482c46da0642a77c3d6
hindhuja2005 <151657743+hindhuja2005@users.noreply.github.com>
2024-11-15 21:23:22 +0530

    Time: 11 ms (87.39%), Space: 30.4 MB (78.5%) - LeetHub

--- 1679-shortest-subarray-to-be-removed-to-make-array-sorted/1679-shortest-subarray-to-be-removed-to-make-array-sorted.py
+++ 1679-shortest-subarray-to-be-removed-to-make-array-sorted/1679-shortest-subarray-to-be-removed-to-make-array-sorted.py
@@ -1,30 +1,17 @@
 class Solution:
     def findLengthOfShortestSubarray(self, arr: List[int]) -> int:
         n = len(arr)
-        
-        # 1. Find the rightmost point where array becomes non-ascending
         right = n - 1
         while right > 0 and arr[right - 1] <= arr[right]:
             right -= 1
-            
-        # 2. If array is already sorted in ascending order
         if right == 0:
             return 0
-            
-        # 3. Initial minimum length is from start to right pointer
         min_length = right
-        
-        # 4. Check each element from left side
         for left in range(n):
-            # 4.1 Break if left sequence becomes non-ascending
             if left > 0 and arr[left - 1] > arr[left]:
                 break
-                
-            # 4.2 Find the first element from right that's >= arr[left]
             while right < n and arr[left] > arr[right]:
                 right += 1
-                
-            # 4.3 Update minimum length of subarray to be removed
             current_length = right - left - 1
             min_length = min(min_length, current_length)
             
