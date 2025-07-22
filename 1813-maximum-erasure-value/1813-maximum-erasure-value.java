12d2c4d707e7f0782dbe04a82194a3ea209d2053
hindhuja2005 <151657743+hindhuja2005@users.noreply.github.com>
2025-07-22 16:42:09 +0530

    Time: 6 ms (97.88%), Space: 60.5 MB (48.78%) - LeetHub

--- 1813-maximum-erasure-value/1813-maximum-erasure-value.java
+++ 1813-maximum-erasure-value/1813-maximum-erasure-value.java
@@ -1,24 +1,23 @@
 class Solution {
     public int maximumUniqueSubarray(int[] nums) {
-        int currentsum = nums[0];
-        int maxsum = nums[0];
-        int right = 1, left = 0;
-        int[] f = new int[10001];
+        int currentsum=nums[0];
+        int maxsum=nums[0];
+        int right=1,left=0;;
+        int f[]=new int[10001];
         Arrays.fill(f, -1); // initialize with -1
         f[nums[0]] = 0;
-
-        while (right < nums.length) {
-            if (f[nums[right]] >= left) {
-                // Remove sum from left to last index of duplicate
-                for (int j = left; j <= f[nums[right]]; j++) {
-                    currentsum -= nums[j];
+        while(right<nums.length){
+            if(f[nums[right]]>=left){
+                for(int j=left;j<=f[nums[right]];j++){
+                    currentsum-=nums[j];
                 }
-                left = f[nums[right]] + 1;
+                left=f[nums[right]]+1;
             }
-            currentsum += nums[right];
-            f[nums[right]] = right;
-            maxsum = Math.max(maxsum, currentsum);
-            right++;
+            f[nums[right]]=right;
+            currentsum+=nums[right++];
+            if (currentsum > maxsum) {
+                maxsum = currentsum;
+            }
         }
         return maxsum;
     }
