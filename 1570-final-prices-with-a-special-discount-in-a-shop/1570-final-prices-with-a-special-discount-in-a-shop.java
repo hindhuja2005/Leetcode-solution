6bab7f13040b473d8970b5d841ca1f3eb7bbb18a
hindhuja2005 <151657743+hindhuja2005@users.noreply.github.com>
2025-08-07 12:27:08 +0530

    Time: 1 ms (99.25%), Space: 43.7 MB (62.47%) - LeetHub

--- 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.java
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.java
@@ -2,14 +2,13 @@ class Solution {
     public int[] finalPrices(int[] prices) {
         int ans[]=new int[prices.length];
         for(int i=0;i<prices.length;i++){
-            int n=prices[i];
+            ans[i]=prices[i];
             for(int j=i+1;j<prices.length;j++){
                 if(prices[j]<=prices[i]){
-                    n=n-prices[j];
+                    ans[i]=ans[i]-prices[j];
                     break;
                 }
             }
-            ans[i]=n;
         }
         return ans;
     }
