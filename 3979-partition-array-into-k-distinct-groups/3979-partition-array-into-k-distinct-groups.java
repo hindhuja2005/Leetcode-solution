73c349f8a615efe3be1ee95bec66cc73242626a7
hindhuja2005 <151657743+hindhuja2005@users.noreply.github.com>
2025-08-25 13:57:14 +0530

    Time: 65 ms (100%), Space: 61.9 MB (100%) - LeetHub

--- 3979-partition-array-into-k-distinct-groups/3979-partition-array-into-k-distinct-groups.java
+++ 3979-partition-array-into-k-distinct-groups/3979-partition-array-into-k-distinct-groups.java
@@ -6,20 +6,6 @@ class Solution {
             f[a]++;
         }
         int group=nums.length/k;
-        /*int sum=0;
-        int count=0;
-        while(count<=nums.length){
-        for(int a:nums){
-            if(f[a]>group){
-                return false;
-            }
-            else{
-                sum+=f[a];
-                f[a]=0;
-                count++;
-            }
-        }
-        }*/
         if(nums.length%k!=0){
             return false;
         }
