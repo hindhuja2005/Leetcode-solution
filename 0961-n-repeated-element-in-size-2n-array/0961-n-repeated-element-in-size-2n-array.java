c9ce3553642370c38c9cf84140c4f9a48281cc5e
hindhuja2005 <151657743+hindhuja2005@users.noreply.github.com>
2026-01-02 18:25:13 +0530

    Time: 263 ms (5.06%), Space: 47.5 MB (50.87%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
@@ -1,6 +1,6 @@
 class Solution {
     public int repeatedNTimes(int[] nums) {
-        int n=(nums.length/2);
+        /*int n=(nums.length/2);
         int l=Integer.MIN_VALUE;
         for(int i:nums){
             l=Math.max(l,i);
@@ -14,6 +14,16 @@ class Solution {
             if(ans[i]==n){
                 res=i;
                 break;
+            }
+        }
+        return res;*/
+        int res=0;
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]==nums[j]){
+                    res=nums[i];
+                    break;
+                }
             }
         }
         return res;
